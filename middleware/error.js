f72fa3324362cd8f55dd392251e32d5fa2d0842a
KamoliddinSaparaliyev <saparaliyevkamoliddin131@gmail.com>
2023-11-27 21:24:15 +0500

    add async and error handler

---
+++ middleware/error.js
@@ -0,0 +1,32 @@
+const { ErrorResponse } = require("../utils/errorResponse");
+
+const errorHandler = (err, req, res, next) => {
+  let error = { ...err }; // Create a shallow copy of the error object
+
+  if (process.env.NODE_ENV === "development") {
+    console.error(err.stack.red);
+  }
+
+  error.message = err.message;
+
+  if (err.name === "CastError") {
+    error = new ErrorResponse(
+      `Resource not found with id of ${err.value}`,
+      404
+    );
+  } else if (err.code === 11000) {
+    error = new ErrorResponse("Duplicate field entered", 400);
+  } else if (err.name === "ValidationError") {
+    error = new ErrorResponse(
+      Object.values(err.errors).map((e) => e.message),
+      400
+    );
+  }
+
+  res.status(error.statusCode || 500).json({
+    success: false,
+    error: error.message || "Internal Server Error",
+  });
+};
+
+module.exports = { errorHandler };
