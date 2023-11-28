5e2b67502192aec2697c5368d1d13d4cb7c0d738
KamoliddinSaparaliyev <saparaliyevkamoliddin131@gmail.com>
2023-11-28 11:34:24 +0500

    fix code

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -4,10 +4,12 @@ const {
   getBootcamp,
   updateBootcamp,
   deleteBootcamp,
+  getBootcampInRadius,
 } = require("../controllers/bootcamps");
 
 const router = require("express").Router();
 
+router.route("/radius/:zipcode/:distance").get(getBootcampInRadius);
 router.route("/").get(getBootcamps).post(postBootcamp);
 router
   .route("/:id")
