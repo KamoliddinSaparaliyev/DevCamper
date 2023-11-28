8d22ed222a0e8e26a3e91631d584ce9929868545
KamoliddinSaparaliyev <saparaliyevkamoliddin131@gmail.com>
2023-11-28 20:07:18 +0500

    fix code and update

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,6 +1,7 @@
 const { asyncHandler } = require("../middleware/async");
-const { ErrorResponse } = require("../utils/errorResponse");
 const { Course } = require("../models/Course");
+const { Bootcamp } = require("../models/Bootcamp");
+const { findResourceById } = require("../utils/findModelById");
 
 /**
  * @desc Get all courses
@@ -31,16 +32,37 @@ exports.getCourses = asyncHandler(async (req, res, nex) => {
  * @param {import("express").Response} res
  * @param {import("express").NextFunction} next
  */
-exports.getCourse = asyncHandler(async (req, res, nex) => {});
+exports.getCourse = asyncHandler(async (req, res, nex) => {
+  await findResourceById(Course, req.params.id);
+
+  const course = await Course.findById(req.params.id).populate(
+    "bootcamp",
+    "name description"
+  );
+
+  res.status(200).json({ success: true, data: course });
+});
+
 /**
  * @desc Create new courses
- * @route /api/v1/courses
+ * @route /api/v1/bootcamps/:bootcampId/courses
  * @access Private
  * @param {import("express").Request} req
  * @param {import("express").Response} res
  * @param {import("express").NextFunction} next
  */
-exports.postCourse = asyncHandler(async (req, res, nex) => {});
+exports.postCourse = asyncHandler(async (req, res, nex) => {
+  const { bootcampId } = req.params;
+
+  req.body.bootcamp = bootcampId;
+
+  await findResourceById(Bootcamp, bootcampId);
+
+  const course = await Course.create(req.body);
+
+  res.status(201).json({ success: true, data: course });
+});
+
 /**
  * @desc Update course
  * @route /api/v1/courses/:id
@@ -49,7 +71,19 @@ exports.postCourse = asyncHandler(async (req, res, nex) => {});
  * @param {import("express").Response} res
  * @param {import("express").NextFunction} next
  */
-exports.updateCourse = asyncHandler(async (req, res, nex) => {});
+exports.updateCourse = asyncHandler(async (req, res, nex) => {
+  const { id } = req.params;
+
+  await findResourceById(Course, id);
+
+  const course = await Course.findByIdAndUpdate(id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(201).json({ success: true, data: course });
+});
+
 /**
  * @desc Delet course
  * @route /api/v1/courses/:id
@@ -58,4 +92,12 @@ exports.updateCourse = asyncHandler(async (req, res, nex) => {});
  * @param {import("express").Response} res
  * @param {import("express").NextFunction} next
  */
-exports.deleteCourse = asyncHandler(async (req, res, nex) => {});
+exports.deleteCourse = asyncHandler(async (req, res, nex) => {
+  const { id } = req.params;
+
+  const course = await findResourceById(Course, id);
+
+  await course.deleteOne();
+
+  res.status(200).json({ success: true, data: course });
+});
