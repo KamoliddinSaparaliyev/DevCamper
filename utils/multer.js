3c033a14ca72be0035514f1bf0396ebf74ebf1ee
KamoliddinSaparaliyev <saparaliyevkamoliddin131@gmail.com>
2023-11-29 21:49:20 +0500

    add multer util for bootcamp photo upload

---
+++ utils/multer.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const fs = require("fs");
+const multer = require("multer");
+
+const uploadDir = path.join(__dirname, "..", "public", "uploads");
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadDir);
+  },
+  filename: function (req, file, cb) {
+    cb(null, "photo_" + Date.now() + "." + file.mimetype.split("/").at(-1));
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/png"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+  },
+  fileFilter: fileFilter,
+});
+module.exports = { upload, uploadDir };
