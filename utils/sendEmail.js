d562b2ba485417100d946a3cda9ce92c3951b4ae
KamoliddinSaparaliyev <saparaliyevkamoliddin131@gmail.com>
2023-12-01 00:36:00 +0500

    add nodemailer package and create util fucnction

---
+++ utils/sendEmail.js
@@ -0,0 +1,31 @@
+const nodemailer = require("nodemailer");
+const { config } = require("../config/config");
+
+const sendEmail = async (options) => {
+  const transporter = nodemailer.createTransport({
+    // host: config.smtp.host,
+    // port: config.smtp.port,
+    // auth: {
+    //   user: config.smtp.email,
+    //   pass: config.smtp.password,
+    // },
+    service: "gmail",
+    auth: {
+      user: "saparaliyevkamoliddin131@gmail.com",
+      pass: "rueybcygmruwzyun",
+    },
+  });
+
+  const message = {
+    from: `${config.smtp.from_name} <${config.smtp.from_email}>`,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+
+  const info = await transporter.sendMail(message);
+
+  console.log("Message sent: %s", info.messageId);
+};
+
+module.exports = { sendEmail };
